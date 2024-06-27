2bd3899345e4efb1516d68cda17024adb93dfd9f
leesujeong <leesujeong@ubisam.com>
2024-06-27 09:45:46 +0900

    ProjectManager 간단 추가

--- src/Components/AdminPage/AdminContent/ProjectManager.js
+++ src/Components/AdminPage/AdminContent/ProjectManager.js
@@ -1,10 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ProjectTable from './ProjectManager/ProjectTable';
+import { GetProjectInfo } from '../../../API/GetProjectInfo';
+import { useSelector } from 'react-redux';
+
 
 const ProjectManager = () => {
+  const isLogged = useSelector(state => state.auth.isLoggedIn);
+  const [projectData, setProjectData] = useState([]);
+  const projects = [
+    {
+      name: 'ELA',
+      type: '1파트',
+      pm: '홍길동',
+      personnel: ['홍길동', '이순신', '강감찬', '유관순', '세종대왕'],
+      status: '시작전',
+      startDate: '2018-09-21 ~ 2019-12-28',
+      View: true,
+      // progress: 100,
+    },
+    {
+      name: '패턴',
+      type: '2파트',
+      pm: 'sysadmin',
+      personnel: ['까치', '둘리', '루피'],
+      status: '시작전',
+      startDate: '2019-07-23 ~ 2021-03-08',
+      View: true,
+      // progress: 72,
+    },
+  ];
+
+  const LoadAllProjectInfo = async () => {
+    const data = await GetProjectInfo("All");
+    if (data === undefined)
+      return;
+    const dataWithIds = data.map((item, index) => ({
+      id: index + 1, // id 필드 추가, 1부터 시작하는 고유한 값
+      ...item
+    }));
+    setProjectData(dataWithIds);
+  }
+
+  useEffect(() => {
+    if (isLogged === true) {
+    LoadAllProjectInfo();
+    }
+  }, [isLogged])
+
   return (
     <div>
-      <h1>Project Manager</h1>
-      <p>This is the Project Manager content.</p>
+      <ProjectTable projects={projectData} />
     </div>
   );
 };
